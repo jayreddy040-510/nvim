fde06b31b4320939b0424b0aedaca5ad62fa2fe9
Jay Reddy <jayreddy@gmail.com>
2023-11-18 13:18:23 -0800

    comments out md-preview and more git fugitive mappings

--- lua/j/packer.lua
+++ lua/j/packer.lua
@@ -1,61 +1,64 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-  use {
-      'nvim-telescope/telescope.nvim', tag = '0.1.3',
-      -- or                            , branch = '0.1.x',
-      requires = { {'nvim-lua/plenary.nvim'} }
-  }
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.3",
+		-- or                            , branch = '0.1.x',
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-  -- Color scheme = onedark "warmer" variant
-  use 'navarasu/onedark.nvim'
-  require('onedark').setup {
-	  style = 'warmer'
-  }
-  require('onedark').load()
+	-- Color scheme = onedark "warmer" variant
+	use("navarasu/onedark.nvim")
+	require("onedark").setup({
+		style = "warmer",
+	})
+	require("onedark").load()
 
-  -- Treesitter
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	-- Treesitter
+	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 
-  -- Harpoon
-  use('theprimeagen/harpoon')
+	-- Harpoon
+	use("theprimeagen/harpoon")
 
-  -- Undotree
-  use('mbbill/undotree')
+	-- Undotree
+	use("mbbill/undotree")
 
-  -- Fugitive for git integration
-  use('tpope/vim-fugitive')
+	-- Fugitive for git integration
+	use("tpope/vim-fugitive")
 
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v2.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},             -- Required
-		  {'williamboman/mason.nvim'},           -- Optional
-		  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" }, -- Required
+			{ "williamboman/mason.nvim" }, -- Optional
+			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},     -- Required
-		  {'hrsh7th/cmp-nvim-lsp'}, -- Required
-		  {'L3MON4D3/LuaSnip'},     -- Required
-	  }
-  }
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "L3MON4D3/LuaSnip" }, -- Required
+		},
+	})
 
-  -- Formatter
-  require('packer').use { 'mhartington/formatter.nvim' }
+	-- Formatter
+	require("packer").use({ "mhartington/formatter.nvim" })
 
-  -- Markdown Preview
-    use({
-        "iamcco/markdown-preview.nvim",
-        run = function() vim.fn["mkdp#util#install"]() end,
-    })
+	-- Markdown Preview
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	})
 
-    use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
-  end)
+	-- use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+end)
