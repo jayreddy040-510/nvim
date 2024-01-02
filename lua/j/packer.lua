ff9374d96dfe553991268f42153d136d6d2819cf
Jay Reddy <jr@care.coach>
2024-01-02 11:25:53 -0800

    adds copilot to packer.lua

--- lua/j/packer.lua
+++ lua/j/packer.lua
@@ -60,5 +60,10 @@ return require("packer").startup(function(use)
 		end,
 	})
 
-	-- use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+	use({
+		"github/copilot.vim",
+		config = function()
+			-- options
+		end,
+	})
 end)
