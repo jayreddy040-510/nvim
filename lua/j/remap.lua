a37ce9f2027c3cc2b04dd487ea3e529b56d2b4e9
Jay Reddy <jr@care.coach>
2024-02-08 13:53:44 -0800

    changes window :vsp remap from <leader>v to <leader>wv

--- lua/j/remap.lua
+++ lua/j/remap.lua
@@ -42,7 +42,7 @@ vim.keymap.set("n", "<leader>tn", ":tabnew<CR>")
 vim.keymap.set("n", "<leader>tx", ":clo<CR>")
 vim.keymap.set("n", "<leader>h", ":noh<CR>")
 vim.keymap.set("n", "<leader>w", "<C-w>")
-vim.keymap.set("n", "<leader>v", ":vsp<CR>")
+vim.keymap.set("n", "<leader>wv", ":vsp<CR>")
 
 -- vim.cmd([[
 -- augroup FormatAutogroup
