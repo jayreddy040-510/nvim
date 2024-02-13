4323f23681b74755e1b55c58ce8745b49f071825
Jay Reddy <jr@care.coach>
2024-02-12 16:30:07 -0800

    adds comment.nvim, adds <leader>wx and adds :vsp to highlight new panel

--- lua/j/remap.lua
+++ lua/j/remap.lua
@@ -42,7 +42,8 @@ vim.keymap.set("n", "<leader>tn", ":tabnew<CR>")
 vim.keymap.set("n", "<leader>tx", ":clo<CR>")
 vim.keymap.set("n", "<leader>hh", ":noh<CR>")
 vim.keymap.set("n", "<leader>w", "<C-w>")
-vim.keymap.set("n", "<leader>wv", ":vsp<CR>")
+vim.keymap.set("n", "<leader>wv", ":vsp<CR><C-w>l", {silent = true})
+vim.keymap.set("n", "<leader>wx", ":clo<CR>")
 
 -- vim.cmd([[
 -- augroup FormatAutogroup
